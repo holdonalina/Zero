d45916977ac25f411351882ba542c5c81b18bee2
holdonalina <elli.born1@gmail.com>
2025-06-15 13:05:04 +0200

    Divide by 0

--- Zero/Program.cs
+++ Zero/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter the first number:");
+bool numberA = int.TryParse(Console.ReadLine(), out var a);
+
+Console.WriteLine("Enter the operator(+, -, * or /):");
+string operators = Console.ReadLine();
+
+Console.WriteLine("Enter the second number:");
+bool numberB = int.TryParse(Console.ReadLine(), out var b);
+
+try
+{
+  if (operators == "/" && b == 0)
+  {
+    throw new Exception("Divide by 0");
+  }
+  double result = operators switch
+  {
+    "+" => a + b,
+    "-" => a - b,
+    "*" => a * b,
+    "/" => a / b,
+  };
+  Console.WriteLine($"{a} {operators} {b} = {result}");
+}
+catch (Exception)
+{
+  Console.WriteLine("Divide by 0 is not possible");
+}
